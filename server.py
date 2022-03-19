52032dcf50d5a422f2615e079bd4117c755c3128
MichaelMulieri <mikemulieri@gmail.com>
2022-03-19 13:06:32 -0400

    initial commit, POST form submission

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+
+
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)  
 
@@ -7,8 +9,19 @@ def index():
 
 @app.route('/checkout', methods=['POST'])         
 def checkout():
+    form_info = {
+        "strawberry" : request.form["strawberry"],
+        "raspberry" : request.form["raspberry"],
+        "apple" : request.form["apple"],
+        "first_name" : request.form["first_name"],
+        "last_name" : request.form["last_name"],
+        "student_id" : request.form["student_id"]
+    }
+    fruit_sum = int(request.form['strawberry']) + int(request.form['raspberry']) + int(request.form['apple'])
+
     print(request.form)
-    return render_template("checkout.html")
+    print(f"Charging {request.form['first_name']} {request.form['last_name']} for {fruit_sum} fruits")
+    return render_template("checkout.html", form=form_info)
 
 @app.route('/fruits')         
 def fruits():
